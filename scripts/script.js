1767be4116e6fe71a30330f7035c99745b8c136f
yuvaraj (Mr_3rr0r_501) <58068070+yuvarajucet@users.noreply.github.com>
2022-08-31 15:02:03 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,30 @@ function changeEye(){
         $('.fa-solid').addClass('fa-eye');
     }
 }
+
+// form submit
+
+function sendHi(){
+    alert('Not implemented yet!')
+    // var name = $('#name')[0].value;
+    // var email = $('#email')[0].value;
+    // var msg = $('#msg')[0].value;
+    // var userMessage = {'name':name,'email':email,'msg':msg};
+
+    // $.ajax({
+    //     url:'',
+    //     method:'post',
+    //     data:userMessage,
+    //     headers:{
+    //         'access-control-allow-origin':'*',
+    //         'access-control-allow-credential':'true'
+    //     },
+    //     success:function(e){
+    //         alert('Thank you, I will contact you soon..');
+    //     },
+    //     error:function(e){
+    //         alert('sorry! something was wrong please try agian later!');
+    //     }
+    // });
+
+};
