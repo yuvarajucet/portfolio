70a44783fe46453967834caed5e8240680be2ffa
yuvaraj (Mr_3rr0r_501) <58068070+yuvarajucet@users.noreply.github.com>
2022-09-04 14:20:52 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,77 @@
-function menuBar(){
-    if($('.checkbtn').css('display') === 'block'){
-        if($('.menuOption').hasClass('showMenuBar')){
-            $('.menuOption').removeClass('showMenuBar');
-        } else{
-            $('.menuOption').addClass('showMenuBar');
-        }
+function menuBar() {
+  if ($(".checkbtn").css("display") === "block") {
+    if ($(".menuOption").hasClass("showMenuBar")) {
+      $(".menuOption").removeClass("showMenuBar");
+    } else {
+      $(".menuOption").addClass("showMenuBar");
     }
+  }
 }
-function changeEye(){
-    if($('.fa-solid').hasClass('fa-eye')){
-        $('.fa-solid').removeClass('fa-eye');
-    } else{
-        $('.fa-solid').addClass('fa-eye');
-    }
+function changeEye() {
+  if ($(".fa-solid").hasClass("fa-eye")) {
+    $(".fa-solid").removeClass("fa-eye");
+  } else {
+    $(".fa-solid").addClass("fa-eye");
+  }
 }
 
 // form submit
 
-$(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() > $(document).height() - 500) {
-        if($('.top').css('display') == 'none'){
-            $('.top').css('display','block');
-        }
-    } else{
-        if($('.top').css('display') == 'block'){
-            $('.top').css('display','none');
-        }
+$(window).scroll(function () {
+  if ($(window).scrollTop() + $(window).height() > $(document).height() - 500) {
+    if ($(".top").css("display") == "none") {
+      $(".top").css("display", "block");
     }
- });
+  } else {
+    if ($(".top").css("display") == "block") {
+      $(".top").css("display", "none");
+    }
+  }
+});
 
-function sendHi(){
-    var name = $('#name')[0].value;
-    var email = $('#email')[0].value;
-    var msg = $('#msg')[0].value;
-    var userData = {
-        "name":name,
-        "email":email,
-        "msg":msg
-    }
-    $('#sayHi').html('sending...');
-    $('#sayHi').attr('disabled',true);
+function sendHi() {
+  var name = $("#name")[0].value;
+  var email = $("#email")[0].value;
+  var msg = $("#msg")[0].value;
+  var userData = {
+    name: name,
+    email: email,
+    msg: msg,
+  };
+  if (name != "" && email != "" && msg != "") {
+    $("#sayHi").html("sending...");
+    $("#sayHi").attr("disabled", true);
 
     $.ajax({
-        url:'https://yuvaraj1221.pythonanywhere.com/support',
-        method:'POST',
-        data:JSON.stringify(userData),
-        headers:{
-            'content-type':'applicatin/json',
-            'access-control-allow-origin':'*',
-            'access-control-allow-credential':'true'
-        },
-        success:function(data){
-            if(data.code == 200){
-                $('#sayHi').html('Sent!');
-            }
-        },
-        error:function(data){
-            $('#sayHi').html('Failed!');
-            $('#sayHi').css('color','red');
+      url: "https://yuvaraj1221.pythonanywhere.com/support",
+      method: "POST",
+      data: JSON.stringify(userData),
+      headers: {
+        "content-type": "applicatin/json",
+        "access-control-allow-origin": "*",
+        "access-control-allow-credential": "true",
+      },
+      success: function (data) {
+        console.log(data);
+        if (data.code == 200) {
+          $("#sayHi").html("Sent!");
         }
+      },
+      error: function (data) {
+        console.log(data);
+        $("#sayHi").html("Failed!");
+        $("#sayHi").css("color", "red");
+      },
     });
-    $('#name')[0].value = '';
-    $('#email')[0].value = '';
-    $('#msg')[0].value = '';
+    $("#name")[0].value = "";
+    $("#email")[0].value = "";
+    $("#msg")[0].value = "";
+    $('#name').css('border-color','green');
+    $('#email').css('border-color','green');
+    $('#msg').css('border-color','green');
+  } else{
+    $('#name').css('border-color','red');
+    $('#email').css('border-color','red');
+    $('#msg').css('border-color','red');
+  }
 }
